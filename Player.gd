257f4282f5521790c2c620a87072930a3245bca5
Tuan Pham <tunaphishy37@gmail.com>
2023-06-21 11:35:03 -0700

    change movement to click to move for now

--- Player.gd
+++ Player.gd
@@ -1,29 +1,18 @@
 extends KinematicBody2D
 
-const ACCELERATION = 3000;
-const DRAG = 3;
-const DEAD_ZONE = 200
+var speed = 400
+onready var target = position
+var velocity = Vector2()
 
-var velocity = Vector2.ZERO;
-var isTouching = false; 
-var directionVector = Vector2(0,0);
-
-# TODO: update to state machine
 func _input(event):
 	if event is InputEventScreenTouch:
-		isTouching = event.is_pressed()
-		directionVector = Vector2(0,0);
-	if event is InputEventScreenDrag and event.speed.length() > DEAD_ZONE:
-		
-		directionVector = event.relative.normalized();
+		print(position)
+		print(event.position)
+		target = get_global_mouse_position()
 
-	
+
 func _physics_process(delta):
-	setVelocity(delta);
-	move_and_slide(velocity);
-	
-func setVelocity(delta):
-	if isTouching:	
-		velocity += directionVector * ACCELERATION * delta;
-	velocity -= velocity * DRAG * delta;
-	velocity.clamped(100);
+	velocity = position.direction_to(target) * speed
+	# look_at(target)
+	if position.distance_to(target) > 5:
+		move_and_slide(velocity)
