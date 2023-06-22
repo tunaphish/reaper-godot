8c4252b7b9ace9050bb2655ac20896973f376dde
Tuan Pham <tunaphishy37@gmail.com>
2023-06-21 18:13:15 -0700

    update site for demo

--- Player.gd
+++ Player.gd
@@ -6,13 +6,10 @@ var velocity = Vector2()
 
 func _input(event):
 	if event is InputEventScreenTouch:
-		print(position)
-		print(event.position)
 		target = get_global_mouse_position()
 
 
 func _physics_process(delta):
 	velocity = position.direction_to(target) * speed
-	# look_at(target)
 	if position.distance_to(target) > 5:
 		move_and_slide(velocity)
